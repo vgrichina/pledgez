a8f0461804d950fdeec0bc4666cc74e219543adc
Vladimir Grichina <vgrichina@componentix.com>
2011-01-15 14:49:40 +0200

    Fixed bugs in implementation, so now it actually works

--- src/main/groovy/com/componentix/pledgez/Pledges.groovy
+++ src/main/groovy/com/componentix/pledgez/Pledges.groovy
@@ -9,19 +9,23 @@ class Pledges {
     }
 
     Pledges addBatch(Map batch) {
-        contexts.addAll(new PledgeContext(batch).childNodes)
+        def newContexts = new PledgeContext(null, "", batch).childNodes
+        newContexts.each { it.parentContext = null }
+        contexts.addAll(newContexts)
 
         return this
     }
 
     Pledges run() {
-        return run({ results, updatedPaths ->
-            println "results = $resultsUpdated, updatedPaths = $updatedPaths"
+        return run({ results, updatedPath ->
+            println "results = $results, updatedPath = $updatedPath"
         } as PledgesRunHandler);
     }
 
     Pledges run(PledgesRunHandler handler) {
-        context.each { it.run(null, [:], handler) }
+        contexts.each { it.run(null, [:], handler) }
+
+        return this
     }
 }
 
@@ -41,7 +45,8 @@ class PledgeContext {
     def topic
     def childNodes = []
 
-    PledgeContext(String name, Map map) {
+    PledgeContext(PledgeContext parentContext, String name, Map map) {
+        this.parentContext = parentContext
         this.name = name
         if (map.containsKey("topic")) {
             this.topicSpecified = true
@@ -51,13 +56,13 @@ class PledgeContext {
             if (value instanceof Closure) {
                 return new Pledge(name: key, closure: value)
             } else {
-                return new PledgeContext(key, value)
+                return new PledgeContext(this, key, value)
             }
         }
     }
 
     def getPath() {
-        (parentContext.path ?: []) + name
+        (parentContext?.path ?: []) + name
     }
 
     void updateResults(Map results, List path, def result) {
@@ -78,23 +83,23 @@ class PledgeContext {
 
     void run(def topic, Map results, PledgesRunHandler handler) {
         if (this.topicSpecified) {
-            topic = this.topic
+            topic = this.topic.call()
         }
 
         childNodes.each { node ->
             if (node instanceof Pledge) {
                 def result = null
                 try {
-                    result = node.closure.run()
+                    result = node.closure.call(topic)
                 } catch (Exception e) {
                     result = e
                 }
 
                 def path = this.path + node.name
                 updateResults(results, path, result)
-                handler.resultsUpdated(topic, results, path)
+                handler.resultsUpdated(results, path)
             } else {
-                node.run(results, handler)
+                node.run(topic, results, handler)
             }
         }
     }
